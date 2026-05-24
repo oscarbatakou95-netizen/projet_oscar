bec6ef33b9ff4cceda69f701ba3f154f8897f223
oscarbatakou95-netizen <oscarbatakou95@gmail.com>
2026-05-24 21:25:58 +0100

    Update database configuration settings

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Configuration de la base de données
-$host = "localhost";
-$dbname = "projetweb2";
-$user = "root";
-$password = "";
+$host = "sql310.infinityfree.com";
+$dbname = "if0_42010333_oscbat";
+$user = "if0_42010333";
+$password = "WSIAQz1N75FBq";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
