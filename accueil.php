6b1028e1f7b6e680ed6e5c6a3ccc05e450f9a73e
oscarbatakou95-netizen <oscarbatakou95@gmail.com>
2026-05-24 21:47:41 +0100

    Enhance accueil.php with viewport meta tag
    Added viewport meta tag for responsive design.

--- accueil.php
+++ accueil.php
@@ -10,6 +10,7 @@ $nom = $_SESSION['user_nom'];
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil - Ma Plateforme</title>
     <link rel="stylesheet" href="style.css">
 </head>
